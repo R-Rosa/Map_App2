41324841c0d80bd4efd2856b85bc878ce8928425
vitor <vitor.rosa.md@gmail.com>
2015-05-07 00:42:28 +0100

    Removed MainActivity class from MapsActivity.java

--- app/src/main/java/com/example/ricardo/map_app/MapsActivity.java
+++ app/src/main/java/com/example/ricardo/map_app/MapsActivity.java
@@ -2,27 +2,10 @@ package com.example.ricardo.map_app;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-package com.example.mapdemo;
-
-import android.app.Activity;
-import android.os.Bundle;
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("MainActivity created!!");
-    }
-}
-
 
 public class MapsActivity extends FragmentActivity {
 
