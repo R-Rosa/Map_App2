9381c9dde300ee05f4734786deab10920c151ac7
R-Rosa <ricardo.jd.rosa@gmail.com>
2015-05-23 20:44:26 +0100

    Evento Click sobre o Marker

--- app/src/main/java/com/example/ricardo/map_app/MapsActivity.java
+++ app/src/main/java/com/example/ricardo/map_app/MapsActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.ricardo.poi.SkatePark;
 import com.google.android.gms.maps.CameraUpdate;
@@ -25,16 +26,38 @@ import java.util.HashMap;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    private HashMap<String,String> SkateParkIds;
+    private HashMap<String,SkatePark> SkateParkIds;
+
+    RelativeLayout relativeLayout=null;
+    ImageView closeButton=null;
+    ImageView backgroundWindow = null;
+    TextView skateParkTitle = null;
+    ImageView skateParkPhotoImageView = null;
+    TextView parkDescriptionTitle = null;
+    TextView parkDescriptionTextView = null;
+    TextView parkDirectionsTitleTextView = null;
+    TextView parkDirectionsTextView = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        SkateParkIds = new HashMap<String,SkatePark>();
 
-        SkateParkIds = new HashMap<String,String>();
 
+        InitializeDetailedWindowViews();
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setVisibility(View.INVISIBLE);
+                mMap.getUiSettings().setScrollGesturesEnabled(true);
+
+            }
+        });
 
 
         /* */
@@ -42,32 +65,33 @@ public class MapsActivity extends FragmentActivity {
         location.setLatitude(38.810149);
         location.setLongitude(-9.332124);
 
+        Location liriosLocation = new Location("Lirios Location");
+        liriosLocation.setLatitude(38.810);
+        liriosLocation.setLongitude(-9.33);
 
-       SkatePark skatePark = new SkatePark();
-       skatePark.setLocation(location);
-       skatePark.setMarkerText("It's my house nigga!!");
-       skatePark.setSnippet(
-               "O parque abre 10:00 e fecha às 17:00.\n" +
-               "Neste momento está fechado para férias.\n"+
-               "O parque está danificado e não se pode andar."
-       );
+        final SkatePark skatePark = new SkatePark(location, null, "Ski Skate Amadora Parque", "Este parque é bué da fixe.",
+                "Condicoes do Park", "Como chegar...", "Ao pé do Continente da Amadora");
+
+        SkatePark lirios = new SkatePark(liriosLocation, null, "Skate Park dos Lírios", "Este parque é ainda melhor.",
+        "Condicoes do Park", "Como Chegar...", "Ao pé do chaby.");
 
         CenterMap(skatePark.getLocation());
-//      PlaceMarker(location);
         PlaceMarker(skatePark);
+        PlaceMarker(lirios);
+
+
+
 
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
                 if(SkateParkIds.containsKey(marker.getId())){
 
-                    String markerType = SkateParkIds.get(marker.getId());
-                    if(markerType == "skatepark"){
-                        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.poiDetailsRelativeLayout);
-                        relativeLayout.setVisibility(View.VISIBLE);
-                        //Lock map
-                        mMap.getUiSettings().setScrollGesturesEnabled(false);
-                    }
+                    SkatePark skatePark = SkateParkIds.get(marker.getId());
+                    UpdateDetailsWindow(skatePark);
+                    relativeLayout.setVisibility(View.VISIBLE);
+                    //Lock map
+                    mMap.getUiSettings().setScrollGesturesEnabled(false);
                 }
 
                 return false;
@@ -75,26 +99,47 @@ public class MapsActivity extends FragmentActivity {
         });
     }
 
+    public void InitializeDetailedWindowViews() {
+        relativeLayout = (RelativeLayout)findViewById(R.id.poiDetailsRelativeLayout);
+        closeButton = (ImageView)findViewById(R.id.closeButton);
+        backgroundWindow = (ImageView)findViewById(R.id.backgroundImageView);
+        skateParkTitle = (TextView)findViewById(R.id.skateParkTitle);
+        skateParkPhotoImageView = (ImageView)findViewById(R.id.skateParkPhotoImageView);
+        parkDescriptionTextView = (TextView)findViewById(R.id.parkDescriptionTextView);
+        parkDescriptionTitle = (TextView)findViewById(R.id.parkDescriptionTitle);
+        parkDirectionsTitleTextView = (TextView)findViewById(R.id.parkDescriptionTitle);
+        parkDirectionsTextView = (TextView)findViewById(R.id.parkDirectionsTextView);
+    }
+
     private void PlaceMarker(SkatePark skatePark) {
         Marker marker = mMap.addMarker(new MarkerOptions()
                         .position(new LatLng(skatePark.getLocation().getLatitude(), skatePark.getLocation().getLongitude()))
-                        .title(skatePark.getMarkerText())
-                        .snippet(skatePark.getSnippet())
                         .draggable(true)
         );
 
-        SkateParkIds.put(marker.getId(), "skatepark");
+        SkateParkIds.put(marker.getId(), skatePark);
     }
 
+    private void UpdateDetailsWindow(SkatePark skatePark)
+    {
+        skateParkTitle.setText(skatePark.getSkateParkTitle());
+        if (skatePark.getSkateParkPhoto()!= null)
+        {
+            skateParkPhotoImageView.setImageBitmap(skatePark.getSkateParkPhoto());
+        }
+        else{
+            //to do - add default image when it doesn't exist (null)
+        }
 
-    private void PlaceMarker(Location location) {
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(location.getLatitude(), location.getLongitude()))
-                .title("Hello world"));
+        skateParkTitle.setText(skatePark.getSkateParkTitle());
+        parkDescriptionTextView.setText(skatePark.getSkateParkTitle());
+        parkDescriptionTitle.setText(skatePark.getParkDescription());
+        parkDirectionsTitleTextView.setText(skatePark.getParkDescriptionTitle());
+        parkDirectionsTextView.setText(skatePark.getParkDirections());
+
+
+
     }
-
-
-
 
     @Override
     protected void onResume() {
@@ -117,6 +162,7 @@ public class MapsActivity extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
--- app/src/main/java/com/example/ricardo/poi/SkatePark.java
+++ app/src/main/java/com/example/ricardo/poi/SkatePark.java
@@ -1,43 +1,99 @@
 package com.example.ricardo.poi;
 
+import android.graphics.Bitmap;
 import android.location.Location;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Ricardo on 08/05/2015.
  */
 public class SkatePark {
 
-    private Location Location;
-    private String MarkerText;
-    private String Snippet;
+    private Location location;
+    private String skateParkTitle;
+    private Bitmap skateParkPhoto;
+    private String parkDescription;
+    private String parkDescriptionTitle;
+    private String parkDirectionsTitle;
+    private String parkDirections;
+    private int markerId;
 
-    public SkatePark(){
-        MarkerText="";
+
+    public SkatePark(Location location, Bitmap skateParkPhoto, String skateParkTitle, String parkDescription, String parkDescriptionTitle, String parkDirectionsTitle,
+                     String parkDirections){
+        this.location = location;
+        this.skateParkPhoto = skateParkPhoto;
+        this.skateParkTitle = skateParkTitle;
+        this.parkDescription = parkDescription;
+        this.parkDescriptionTitle = parkDescriptionTitle;
+        this.parkDirectionsTitle = parkDirectionsTitle;
+        this.parkDirections = parkDirectionsTitle;
+        this.parkDirections = parkDirections;
     }
-
 
     //location
     public Location getLocation() {
-        return Location;
+        return location;
     }
     public void setLocation(Location location) {
-        this.Location = location;
+        this.location = location;
     }
 
-    //MarkerText
-    public String getMarkerText() {
-        return MarkerText;
-    }
-    public void setMarkerText(String markerText) {
-        if(markerText!=null)
-            MarkerText = markerText;
+    public String getSkateParkTitle() {
+        return skateParkTitle;
     }
 
-    //Snippet
-    public String getSnippet() {
-        return Snippet;
+    public void setSkateParkTitle(String skateParkTitle) {
+        this.skateParkTitle = skateParkTitle;
     }
-    public void setSnippet(String snippet) {
-        this.Snippet = snippet;
+
+    public Bitmap getSkateParkPhoto() {
+        return skateParkPhoto;
+    }
+
+    public void setSkateParkPhoto(Bitmap skateParkPhoto) {
+        this.skateParkPhoto = skateParkPhoto;
+    }
+
+    public String getParkDescription() {
+        return parkDescription;
+    }
+
+    public void setParkDescription(String parkDescription) {
+        this.parkDescription = parkDescription;
+    }
+
+    public String getParkDescriptionTitle() {
+        return parkDescriptionTitle;
+    }
+
+    public void setParkDescriptionTitle(String parkDescriptionTitle) {
+        this.parkDescriptionTitle = parkDescriptionTitle;
+    }
+
+    public String getParkDirectionsTitle() {
+        return parkDirectionsTitle;
+    }
+
+    public void setParkDirectionsTitle(String parkDirectionsTitle) {
+        this.parkDirectionsTitle = parkDirectionsTitle;
+    }
+
+    public String getParkDirections() {
+        return parkDirections;
+    }
+
+    public void setParkDirections(String parkDirections) {
+        this.parkDirections = parkDirections;
+    }
+
+    public int getMarkerId() {
+        return markerId;
+    }
+
+    public void setMarkerId(int markerId) {
+        this.markerId = markerId;
     }
 }
